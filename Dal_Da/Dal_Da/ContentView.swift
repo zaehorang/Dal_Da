416784a85f7cbcc16a88e349249a81476851ca5d
zaehorang <zaehorang2@naver.com>
2024-04-13 15:04:29 +0900

    chore: 프로젝트 생성

---
+++ Dal_Da/Dal_Da/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Dal_Da
+//
+//  Created by zaehorang on 4/13/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Dal_Da/Dal_Da/Dal_DaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dal_DaApp.swift
+//  Dal_Da
+//
+//  Created by zaehorang on 4/13/24.
+//
+
+import SwiftUI
+
+@main
+struct Dal_DaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
