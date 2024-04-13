e3a896ed76b9c0d3ecf59ebe706ebc5543a7ddf1
zaehorang <zaehorang2@naver.com>
2024-04-13 15:14:51 +0900

    feat: add SplashView
    - image 추가
    - view 전환

---
+++ Dal_Da/Dal_Da/SplashView.swift
@@ -0,0 +1,38 @@
+//
+//  SplashView.swift
+//  Dal_Da
+//
+//  Created by zaehorang on 4/13/24.
+//
+
+import SwiftUI
+
+struct SplashView: View {
+    
+    @State var isActive: Bool = false
+    
+    var body: some View {
+        ZStack {
+            if self.isActive {
+                ContentView()
+            } else {
+                Image("Splash")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .edgesIgnoringSafeArea(.top)
+            }
+        }
+        .onAppear {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 2.5) {
+                withAnimation {
+                    self.isActive = true
+                }
+            }
+        }
+    }
+    
+}
+
+#Preview {
+    SplashView()
+}
